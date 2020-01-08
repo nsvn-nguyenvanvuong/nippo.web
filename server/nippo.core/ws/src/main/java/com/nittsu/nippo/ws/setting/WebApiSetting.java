94c0789f88e008b661400b98ee366c9a8bbc1ea8
hoangdc-nittsusystem <hoang_dc@nittsusystem-vn.com>
2020-01-08 13:52:54 +0700

    update api

--- server/nippo.core/ws/src/main/java/com/nittsu/nippo/ws/setting/WebApiSetting.java
+++ server/nippo.core/ws/src/main/java/com/nittsu/nippo/ws/setting/WebApiSetting.java
@@ -3,5 +3,5 @@ package com.nittsu.nippo.ws.setting;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/webapi")
 public class WebApiSetting extends Application {}
