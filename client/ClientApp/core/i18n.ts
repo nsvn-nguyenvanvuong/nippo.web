33d5c0579c0edf0ccb62a0e4fa8c96e6d5274d9d
Nguyễn Tuệ Vương <vungtroixa@gmail.com>
2020-02-01 00:40:54 +0700

    Update i18n.ts

--- client/ClientApp/core/i18n.ts
+++ client/ClientApp/core/i18n.ts
@@ -2,7 +2,7 @@ import { _, $, ko } from 'core/providers';
 import { handler, component } from 'core/decorators';
 
 const lang = ko.observable('vi')
-    , resource = {
+    , resources: { [lg: string]: { [key: string]: string } } = {
         vi: {
             'vi': 'Tiếng Việt',
             'en': 'English',
@@ -17,19 +17,50 @@ const lang = ko.observable('vi')
             'profile': 'Profile',
             'home': 'Home'
         }
+    }
+    , getText = (lg: string, resource: string | number, params?: string | string[] | { [key: string]: string }): string => {
+        const groups: { [key: string]: string } = {}
+            , regex: RegExp = /{\d+}|#{[^}]+}|{#[^}]+}/g
+            , i18lang: { [key: string]: string } = resources[lg];
+
+        // accept numbet raw
+        resource = (resource || '').toString();
+
+        if (!_.isNull(params)) {
+            if (_.isNumber(params)) {
+                params = params.toString();
+            }
+
+            if (!_.isString(params)) {
+                _.extend(groups, params);
+            } else {
+                _.extend(groups, { 0: params.toString() });
+            }
+        }
+
+        if (!_.isNil(resource)) {
+            return (i18lang[resource] || resource)
+                .replace(regex, (match: string) => {
+                    const key = match.replace(/[#{}]/g, '');
+
+                    return getText(lg, (groups[key] || key), groups);
+                }) || resource;
+        }
+
+        return '';
     };
 
 @handler({
     bindingName: 'i18n'
 })
 export class I18nBinding implements ko.BindingHandler {
-    init(el: HTMLElement, accessor: () => ko.BindingAccessors | string) {
+    init(el: HTMLElement, accessor: () => ko.Observable<string> | string) {
         ko.computed({
             read() {
                 const lg = ko.toJS(lang)
                     , res = ko.toJS(accessor());
 
-                $(el).text(_.get(resource, `${lg}.${res}`) || res);
+                $(el).text(getText(lg, res));
             },
             disposeWhenNodeIsRemoved: el
         });
